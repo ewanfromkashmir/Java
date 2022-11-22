9805fcef42d0d61d41723609096596d04673fe30
ewanhughes26 <79140539+ewanhughes26@users.noreply.github.com>
2022-11-22 17:03:53 +0000

    Updated username program such that the Admin credentials can be attempted 3 times before the system is locked out. A bug currently exists where a password can only be inputted if the username is "admin".

--- src/Username.java
+++ src/Username.java
@@ -5,7 +5,6 @@ public class Username
     public static void main(String[] args)
     {
         String username;
-
         Scanner reader = new Scanner(System.in);
 
         do
@@ -14,6 +13,38 @@ public class Username
             username = reader.nextLine();
         }
         while (username.isEmpty());
+
+        String password;
+        int counter = 0;
+
+        while ((username.toLowerCase().equals("admin")) && (counter < 3))
+        {
+            System.out.print("Enter Password: ");
+            reader = new Scanner(System.in);
+            password = reader.nextLine();
+
+            if (password.equals("PaSSword!!!"))
+            {
+                System.out.println();
+                System.out.println("Access Granted.");
+                return;
+            }
+
+            else
+            {
+                System.out.println();
+                System.out.println("Access Denied.");
+                counter++;
+                System.out.println((3 - counter) + " attempts remaining." );
+                System.out.println();
+            }
+        }
+
+        System.out.println();
+        System.out.println("System locked. Please contact an administrator.");
+
+
+
 
 
     }
